fa3bc5f70e6979434451b19827f6b8fdd1577863
lavan <lavan@LAPTOP-638FO4FI>
2026-08-17 10:29:58 +0530

    init commit

---
+++ src/test/java/org/example/servletintellij/ServletIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servletintellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletIntellijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
